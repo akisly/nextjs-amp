2f4d08ff6bb7d659ab691af1b6e2552a53fe84f9
Aleksandr Kisliy <oleksandr.kyslyi@onix-systems.com>
2020-12-19 23:17:34 +0200

    amp

--- pages/category/[cid].js
+++ pages/category/[cid].js
@@ -21,11 +21,8 @@ Category.getInitialProps = async ({ query }) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=1`);
   let posts = await res.json();
 
-  posts.map(post => {
-    return (post.category = categories[Math.floor(Math.random() * categories.length)]);
-  });
-  
-  let category = categories.find(cat => cat.id === query.cid);
+  posts.forEach((post) => (post.category = categories[Math.floor(Math.random() * categories.length)]));
+  const category = categories.find((cat) => cat.id == query.cid);
   posts = posts.filter(post => post.category.id === category.id);
   
   return { category, posts };
